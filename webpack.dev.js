9006698df758dcddfe2418cb6724022dfa415117
kroxon <kadetkroksapsp2@gmail.com>
2024-12-09 20:30:20 +0100

    Create tensorflowjs file
    Created basic functions to export.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,16 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const path = require("path");
 
 module.exports = merge(common, {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/template.html"],
+    watchFiles: ["./src/template.html"], 
+    static: {
+      directory: path.resolve(__dirname, "dist"), 
+    },
+    port: 8080, 
+    open: true, 
   },
 });
