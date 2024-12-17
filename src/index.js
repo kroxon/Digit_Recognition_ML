d275c426f48816b8b95c28fb425f970e4fea53b4
kroxon <123456789+odin@users.noreply.github.com>
2024-12-17 18:39:30 +0100

    Train model

--- src/index.js
+++ src/index.js
@@ -37,35 +37,69 @@ canvas.addEventListener("mouseup", () => {
 
 canvas.addEventListener("mouseout", () => {
     isDrawing = false;
-    canvas.style.cursor = 'default'; // przywrócenie normalnego kursora
+    canvas.style.cursor = 'default';
 });
 
-
-
-
 async function loadData() {
-    // Załaduj dane z plików JSON
     const XResponse = await fetch('data/X.json');
     const yResponse = await fetch('data/y.json');
 
-    // Przekształć odpowiedzi na obiekty JSON
     let X = await XResponse.json();
-    const y = await yResponse.json();
+    let y = await yResponse.json();
 
     X = X.map(row => row.map(value => value > 0.5 ? 1 : 0));
 
-    // Zwróć dane
+    if (Array.isArray(y) && Array.isArray(y[0])) {
+        y = y.map(row => row[0]); // Przyjmujemy, że interesuje nas pierwsza wartość w każdej podtablicy
+    }
+
+    X = tf.tensor2d(X); // Konwersja X na tensor 2D
+    y = tf.tensor1d(y); // Konwersja y na tensor 1D
+
     return { X, y };
 }
 
-// Przykład użycia:
-loadData().then(({ X, y }) => {
-    // console.log('X:', X[7]);
-    // console.log('y:', y);
-    // console.log('X:', X[0]);
-    console.log('The first element of y is:', y[0][0]);
-    console.log('The last element of y is:', y[y.length - 1][0]);
+function createModel() {
+    const model = tf.sequential();
+    model.add(tf.layers.dense({ units: 25, activation: 'relu', inputShape: [400], name: 'L1' }));
+    model.add(tf.layers.dense({ units: 15, activation: 'relu', name: 'L2' }));
+    model.add(tf.layers.dense({ units: 10, activation: 'linear', name: 'L3' }));
 
-    console.log('The shape of X is: [' + X.length + ', ' + (X[0] ? X[0].length : 0) + ']');
-    console.log('The shape of y is: [' + y.length + ', ' + (y[0] ? y[0].length : 0) + ']');
-});
+    return model;
+}
+
+async function trainModel(model, X, y) {
+    model.compile({
+        loss: 'sparseCategoricalCrossentropy',
+        optimizer: tf.train.adam(0.001),
+        metrics: ['accuracy']
+    });
+
+    const history = await model.fit(X, y, {
+        epochs: 40,
+        batchSize: 32, // Możesz dostosować rozmiar partii
+        shuffle: true, // Losowe tasowanie danych
+        callbacks: {
+            onEpochEnd: (epoch, logs) => {
+                console.log(`Epoka ${epoch + 1}: loss = ${logs.loss.toFixed(4)}, accuracy = ${logs.acc.toFixed(4)}`);
+            }
+        }
+    });
+
+    return history;
+}
+
+async function main() {
+    try {
+        const { X, y } = await loadData();
+
+        const model = createModel();
+
+        const history = await trainModel(model, X, y);
+        console.log('Trening zakończony!', history);
+    } catch (err) {
+        console.error('Błąd podczas ładowania danych lub trenowania modelu:', err);
+    }
+}
+
+main();
