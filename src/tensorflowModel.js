ee1b7c7749ffaecea9530ebec364f3df6c106a4b
kroxon <kadetkroksapsp2@gmail.com>
2024-12-17 15:32:54 +0100

    Change drawing function to canvas

--- src/tensorflowModel.js
+++ src/tensorflowModel.js
@@ -4,26 +4,32 @@ let model = null;
 
 // Funkcja ładowania modelu
 async function loadModel() {
-    if (!model) {
-        model = await tf.loadLayersModel("./models/model.json");
-        console.log("Model załadowany:", model.summary());
+    try {
+        const modelPath = "./models/model.json";
+        const loadedModel = await tf.loadLayersModel(modelPath);
+
+
+        console.log("Model załadowany poprawnie:", model.summary());
+        return model;
+    } catch (error) {
+        console.error("Błąd przy ładowaniu modelu:", error);
     }
-    return model;
 }
 
 // Funkcja predykcji
 async function predictDigit(data) {
     try {
+        const model = await loadModel();
+
         // Tworzenie tensora wejściowego o kształcie [1, 400] - 1 próbka, 400 cech
-        const inputShape = [1, 400]; 
+        const inputShape = [1, 400];
         const inputTensor = tf.tensor(data, inputShape);
 
-        const model = await loadModel();
+        // Wykonanie predykcji
         const prediction = model.predict(inputTensor);
 
-        // Normalizacja wyników predykcji
-        const probabilities = prediction.softmax();
-        const predictedClass = probabilities.argMax(1).dataSync()[0];
+        // Wybranie klasy o najwyższym prawdopodobieństwie
+        const predictedClass = prediction.argMax(1).dataSync()[0];
 
         console.log("Prediction:", predictedClass);
         return predictedClass;
