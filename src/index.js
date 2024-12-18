8c7f8148b8965ce095f365ebdb631b813284d454
kroxon <123456789+odin@users.noreply.github.com>
2024-12-19 00:49:23 +0100

    Saved and loaded model
    Pretrained model was saved and loaded successfully.
    Training the model was not done correctly.
    Need to fix the training process.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.css';
-import { loadData, createModel, trainModel, predictDigit } from './model';
+import { loadData, loadModel, predictDigit } from './model';
 // import dataX from './data/X.json';
 // import dataY from './data/y.json';
 
@@ -45,48 +45,27 @@ canvas.addEventListener("mouseout", () => {
     canvas.style.cursor = 'default';
 });
 
-// async function loadData() {
-//     const XResponse = await fetch('data/X.json');
-//     const yResponse = await fetch('data/y.json');
-
-//     let X = await XResponse.json();
-//     let y = await yResponse.json();
-
-//     X = X.map(row => row.map(value => value > 0.5 ? 1 : 0));
-
-//     if (Array.isArray(y) && Array.isArray(y[0])) {
-//         y = y.map(row => row[0]);
-//     }
-
-//     X = tf.tensor2d(X);
-//     y = tf.tensor1d(y);
-
-//     return { X, y };
-// }
-
-// function createModel() {
-//     const model = tf.sequential();
-//     model.add(tf.layers.dense({ units: 128, activation: 'relu', inputShape: [400] }));
-//     model.add(tf.layers.dropout({ rate: 0.5 }));
-//     model.add(tf.layers.dense({ units: 64, activation: 'relu' }));
-//     model.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
-
-//     return model;
-// }
 
 async function main() {
+    let model;
     try {
         const { X, y } = await loadData();
 
-        const model = createModel();
+        // model = createModel();
 
-        const history = await trainModel(model, X, y);
-        console.log('Finished training:', history);
+        // const history = await trainModel(model, X, y);
+        // console.log('Finished training:', history);
 
-        await predictDigit(X, model, 2015); // Predict a example digit
+        // Load the pre-trained model
+        model = await loadModel('/models/model.json');
+
+        await predictDigit(X, model,4615); // Predict a example digit
     } catch (err) {
         console.error('Error during training:', err);
     }
+
+
+
 }
 
 main();
