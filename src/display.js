89f14fc2285ac0623e38e48614ee1e6fa8176563
kroxon <kadetkroksapsp2@gmail.com>
2024-12-20 11:26:00 +0100

    Create canvas.js file
    Create canvas.js file for drawing the canvas and the drawing functions.

--- src/display.js
+++ src/display.js
@@ -1,4 +1,5 @@
 export function displayDigit(imageOfDigit) {
+    
     const reshapedImage = imageOfDigit.reshape([20, 20]);   // Shape the image to 20x20
     const imageArray = reshapedImage.arraySync();   // Convert to array
 
@@ -8,15 +9,8 @@ export function displayDigit(imageOfDigit) {
 
     let container = document.getElementById('digitGrid');
     if (!container) {
-        console.log("Creating container...");
         container = document.createElement('div');
         container.id = 'digitGrid';
-        container.style.display = 'grid';
-        container.style.gridTemplateColumns = 'repeat(20, 1fr)';
-        container.style.gridTemplateRows = 'repeat(20, 1fr)';
-        container.style.width = '200px';
-        container.style.height = '200px';
-        container.style.gap = '1px';
         document.body.appendChild(container);
     }
 
